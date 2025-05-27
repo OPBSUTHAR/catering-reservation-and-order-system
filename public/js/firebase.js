c1725ba17de3978ff7270773322714fe6d5ffead
omprakashsuthar <omprakashsuhar@gmail.com>
2025-05-27 17:25:40 +0530

    updated style and html content with alteration

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -2,19 +2,23 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyCTMagK8zneRb7DjWbFO9CgV03z7Khmy3g",
+  authDomain: "cateringreservationorderingsys.firebaseapp.com",
+  projectId: "cateringreservationorderingsys",
+  storageBucket: "cateringreservationorderingsys.firebasestorage.app",
+  messagingSenderId: "737129861298",
+  appId: "1:737129861298:web:9f35ebc42b566b475039ab",
+  measurementId: "G-F7FYE2NDF3"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Initialize Firebase services
 const auth = getAuth(app);
@@ -22,4 +26,4 @@ const db = getFirestore(app);
 const storage = getStorage(app);
 
 // Export the services
-export { auth, db, storage };
+export { auth, db, storage, analytics };
